36b4b118cd95fe0d393b90035f63b1bed7f61e20
polojerry <polojerry4@gmail.com>
2020-04-29 17:51:54 +0300

    Added directory input manually and added a validity check

--- renamer.py
+++ renamer.py
@@ -3,7 +3,7 @@ from os import path
 
 
 def main():
-    file_dir = "/home/polo/Pictures/Delvis Photos/whiskey/m--->>"
+    file_dir = input("Enter Images Directory:\n")
     if path.exists(file_dir):
         file_names = os.listdir(file_dir)
         jpeg_files_names = [_ for _ in file_names if _[-5:] == ".jpeg"]
@@ -11,6 +11,8 @@ def main():
             src_name = file_dir + "/" + file_name
             dest_name = file_dir + "/" + file_name.lower().replace(" ", "_")
             os.rename(src_name, dest_name)
+    else:
+        print("Directory Does Not Exist")
 
 
 if __name__ == '__main__':
